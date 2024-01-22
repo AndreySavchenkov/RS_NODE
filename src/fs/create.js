5243991cd5d75852c435b8f7368171d42c85b994
AndreySavchenkov <sannndvik16@gmail.com>
2024-01-22 11:33:51 +0100

    feat: add create fc

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+
+const fileContent = "I am fresh and young";
+const folderPath = "src/fs/files";
+const fileName = "fresh.txt";
+
 const create = async () => {
-    // Write your code here 
+  const filePath = path.join(folderPath, fileName);
+
+  try {
+    await fs.access(filePath);
+    throw new Error("FS operation failed");
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      await fs.writeFile(filePath, fileContent);
+      console.log(`File created successfully: ${fileName}`);
+    } else {
+      throw error;
+    }
+  }
 };
 
 await create();
