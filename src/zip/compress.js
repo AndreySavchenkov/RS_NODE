b4f6c4ce3c1fc7ec473c5d637332fb097b0c966f
AndreySavchenkov <sannndvik16@gmail.com>
2024-01-24 10:41:14 +0100

    feat: add decompress fc

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,25 +1,21 @@
-import zlib from 'zlib';
-import fs from 'fs';
-import { pipeline } from 'stream/promises';
+import zlib from "zlib";
+import fs from "fs";
+import { pipeline } from "stream/promises";
 
-const fileToCompress = 'src/zip/files/fileToCompress.txt';
-const compressedFile = 'src/zip/files/archive.gz';
+const fileToCompress = "src/zip/files/fileToCompress.txt";
+const compressedFile = "src/zip/files/archive.gz";
 
 const compress = async () => {
-    const readStream = fs.createReadStream(fileToCompress);
-    const gzipStream = zlib.createGzip();
-    const writeStream = fs.createWriteStream(compressedFile);
+  const readStream = fs.createReadStream(fileToCompress);
+  const gzipStream = zlib.createGzip();
+  const writeStream = fs.createWriteStream(compressedFile);
 
-    try {
-        await pipeline(
-          readStream,
-          gzipStream,
-          writeStream
-        );
-        console.log('Compression completed');
-      } catch (error) {
-        console.error('Compression failed:', error);
-      }
+  try {
+    await pipeline(readStream, gzipStream, writeStream);
+    console.log("Compression completed");
+  } catch (error) {
+    console.error("Compression failed:", error);
+  }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
+import zlib from "zlib";
+import fs from "fs";
+import { pipeline } from "stream/promises";
+
+const compressedFile = "src/zip/files/archive.gz";
+const decompressedFile = "src/zip/files/decompressedFile.txt";
+
 const decompress = async () => {
-    // Write your code here 
+  const readStream = fs.createReadStream(compressedFile);
+  const writeStream = fs.createWriteStream(decompressedFile);
+  const gunzipStream = zlib.createGunzip();
+
+  try {
+    await pipeline(readStream, gunzipStream, writeStream);
+    console.log("Decompression completed");
+  } catch (error) {
+    console.error("Decompression failed:", error);
+  }
 };
 
 await decompress();
