2dd16953c1278761c37df1f62980fdca08afa421
AndreySavchenkov <sannndvik16@gmail.com>
2024-01-24 10:36:21 +0100

    feat: add compress fc

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,25 @@
+import zlib from 'zlib';
+import fs from 'fs';
+import { pipeline } from 'stream/promises';
+
+const fileToCompress = 'src/zip/files/fileToCompress.txt';
+const compressedFile = 'src/zip/files/archive.gz';
+
 const compress = async () => {
-    // Write your code here 
+    const readStream = fs.createReadStream(fileToCompress);
+    const gzipStream = zlib.createGzip();
+    const writeStream = fs.createWriteStream(compressedFile);
+
+    try {
+        await pipeline(
+          readStream,
+          gzipStream,
+          writeStream
+        );
+        console.log('Compression completed');
+      } catch (error) {
+        console.error('Compression failed:', error);
+      }
 };
 
 await compress();
