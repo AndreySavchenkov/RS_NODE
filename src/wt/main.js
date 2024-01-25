494e40edd5cab399ad81d291d4e821d6948824ea
AndreySavchenkov <sannndvik16@gmail.com>
2024-01-25 09:53:06 +0100

    feat: add worker threads functions

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,39 @@
+import { Worker } from "worker_threads";
+import os from "os";
+
+const numCPUs = os.cpus().length;
+
+const createWorker = async (id) => {
+  return new Promise((resolve, reject) => {
+    //TODO: refactor path
+    const worker = new Worker("./src/wt/worker.js", { workerData: id });
+
+    worker.on("message", (message) => {
+      console.log(`Received result from Worker ${id}:`, message);
+      resolve(message);
+    });
+
+    worker.on("error", (error) => {
+      console.error(`Error in Worker ${id}:`, error);
+      reject({ status: "error", data: null });
+    });
+  });
+};
+
 const performCalculations = async () => {
-    // Write your code here
+  const workerPromises = [];
+
+  for (let i = 0; i < numCPUs; i++) {
+    const inputData = 10 + i;
+    workerPromises.push(createWorker(inputData));
+  }
+
+  try {
+    const results = await Promise.all(workerPromises);
+    console.log("All workers finished. Results:", results);
+  } catch (error) {
+    console.error("Error in one or more workers:", error);
+  }
 };
 
 await performCalculations();
